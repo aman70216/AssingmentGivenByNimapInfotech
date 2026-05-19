a11f077fe5220d724834f643e7e063d2ac349e57
Aman DeviPrakash Mishra <125070627+aman70216@users.noreply.github.com>
2026-05-19 18:17:10 +0530

    Update comment for DATABASE_URL in database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:992Aman*@localhost:3306/ProductAndCategory" #Url of ProductAndCategory DB Use Here for Connection If Want TO Run Locally Replace Root with Your UserName and 992Aman with your Password
+DATABASE_URL = "mysql+pymysql://root:992Aman*@localhost:3306/ProductAndCategory" #Url of ProductAndCategory DB Use Here for Connection If You Want TO Run Locally Replace Root with Your UserName and 992Aman with your Password
 
 Connect= create_engine(DATABASE_URL)
 
