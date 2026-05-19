d5c2741c0af473b25605a637c8d0bd2376123280
Aman DeviPrakash Mishra <125070627+aman70216@users.noreply.github.com>
2026-05-19 18:04:23 +0530

    Clarify DATABASE_URL usage in database.py
    Updated DATABASE_URL comment to provide clearer instructions for local usage.

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:992Aman*@localhost:3306/ProductAndCategory" #Url of ProductAndCategory DB Use Here for Connection
+DATABASE_URL = "mysql+pymysql://root:992Aman*@localhost:3306/ProductAndCategory" #Url of ProductAndCategory DB Use Here for Connection If Want TO Run Locally Replace Root with Your UserName and 992Aman with your Password
 
 engine = create_engine(DATABASE_URL)
 
