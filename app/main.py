390cc66975ba994f07b8e7b5c41c0c18f2035404
Aman DeviPrakash Mishra <125070627+aman70216@users.noreply.github.com>
2026-05-19 18:08:35 +0530

    Change database connection from engine to Connect
    Updated database connection to use Connect instead of engine.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from app.database import engine, Base
+from app.database import Connect, Base
 from app.routers import category, product
 
-Base.metadata.create_all(bind=engine) # Connecting to Database ProductAndCategory Of Mysql 
+Base.metadata.create_all(bind=Connect) # Connecting to Database ProductAndCategory Of Mysql 
 
 app = FastAPI()
 
